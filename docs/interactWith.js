4925d6deb5d96921d2d0579e70afefb828d3dbe1
Lyndon Herschell <114019791+LPHerschell@users.noreply.github.com>
2024-11-11 22:47:10 -0500

    DIGIT 400 1 (part 2)

--- docs/interactWith.js
+++ docs/interactWith.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', bunny, false);
 
 function bunny() {
-    alert ('Hi there! Looks like the page loaded! Yay!');
+    alert ('Welcome to my website! Looks like the page loaded...almost! Just click here to continue, please.');
     var buttons = document.getElementsByTagName("button")
     buttons[0].addEventListener('click', changeColor, false);
     buttons[1].addEventListener('click', changeColor2, false);
@@ -15,13 +15,14 @@ function changeColor() {
     { p1.style.backgroundColor = "skyblue";  }
     
     /* here, style is a *property*: the CSS styling of an element: 
-     * you can add a CSS property after invoking style. */
+     * you can add a CSS property after invoking style. */ 
+    // ^I will keep this in mind for the future
 }
 
 function changeColor2() {
     var pars = document.getElementsByTagName('p')
     for (var i = 0, length = pars.length; i < length; i++) {
-        pars[i].style.backgroundColor = "pink";
+        pars[i].style.backgroundColor = "lightcoral";
         pars[i].style.fontWeight="bold";
     }
 }
@@ -29,16 +30,16 @@ function changeColor2() {
 
 
 function newFunction() {
-    alert ('YO! This is function is firing!!');
+    alert ('Huh? What do you want now?');
     var li = document.getElementsByTagName('li')
     for (var i = 0, length = li.length; i < length; i++) {
-        li[i].style.backgroundColor = "green";
+        li[i].style.backgroundColor = "MistyRose";
     }
 }
 
 
 function anotherFunction() {
-    var anothers = document.getElementsByClassName('stuff')
+    var anothers = document.getElementsByClassName('power')
     for (var i = 0, length = anothers.length; i < length; i++) {
         anothers[i].style.cssText = "text-decoration: underline; text-decoration-style: wavy;"
     }
